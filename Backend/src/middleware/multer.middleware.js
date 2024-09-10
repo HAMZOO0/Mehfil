57dabfdf6a65ec99fbe425f25faa797c6a313f91
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-10 22:47:39 +0500

    Update multer.middleware.js

--- Backend/src/middleware/multer.middleware.js
+++ Backend/src/middleware/multer.middleware.js
@@ -1,16 +1,37 @@
-import multer from "multer";
+import express from "express";
+import cookieparser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public");
-  },
+const app = express();
 
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+app.use(express.json({ limit: "16 kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16 kb" }));
+app.use(express.static(path.join(__dirname, 'public'))); // Correct usage of __dirname
+app.use(cookieparser());
 
-export const upload = multer({ storage });
+app.use(
+  cors({
+    origin: 'https://mehfil-social-media.vercel.app', // Corrected quotation mark
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
 
-// more about multer
-// https://github.com/expressjs/multer
+import user_router from "./routers/user.route.js";
+import post_router from "./routers/post.route.js";
+import like_router from "./routers/like.route.js";
+import follow_router from "./routers/follow.route.js";
+import comment_router from "./routers/comment.route.js";
+import bookmark_router from "./routers/bookmark.route.js";
+import healthcheck_router from "./routers/healthcheck.router.js";
+
+app.use("/api/v1/users", user_router);
+app.use("/api/v1/posts", post_router);
+app.use("/api/v1/likes", like_router);
+app.use("/api/v1/follow", follow_router);
+app.use("/api/v1/comment", comment_router);
+app.use("/api/v1/bookmark", bookmark_router);
+app.use("/api/v1/healthcheck", healthcheck_router);
+
+export { app };
