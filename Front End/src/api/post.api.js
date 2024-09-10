c2a5a5818301302b073e39e0e4df8e7ee70d827b
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-10 01:55:21 -0400

    Update post.api.js

--- Front End/src/api/post.api.js
+++ Front End/src/api/post.api.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import { BASE_URL } from "../../config variables.js";
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://mehfil-seven.vercel.app/api/v1/",
   withCredentials: true, // Include credentials with requests
 });
 
