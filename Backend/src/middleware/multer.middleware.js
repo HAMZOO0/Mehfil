0f709b4c3ecf504b809beb4b7c2fab8148c1dca6
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-10 23:05:37 +0500

    Update multer.middleware.js

--- Backend/src/middleware/multer.middleware.js
+++ Backend/src/middleware/multer.middleware.js
@@ -1,16 +1,21 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public");
+    const uploadPath = path.join(__dirname, 'public');
+    console.log(`Uploading to: ${uploadPath}`);
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+    cb(null, uploadPath);
   },
 
   filename: function (req, file, cb) {
+    console.log(`Saving file: ${file.originalname}`);
     cb(null, file.originalname);
   },
 });
 
 export const upload = multer({ storage });
-
-// more about multer
-// https://github.com/expressjs/multer
