40e24481997769734afb41c8872fd748c17421e2
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-11 11:24:49 -0400

    Update Header.jsx

--- Front End/src/components/Header/Header.jsx
+++ Front End/src/components/Header/Header.jsx
@@ -24,45 +24,55 @@ const Header = () => {
           </div>
         </div>{" "}
       </Link>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        class="h-12 w-64 md:w-96 px-4 pr-10 rounded-lg border shadow-sm bg-gray-700 border-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:w-[700px] transition-all duration-300"
+      />
+      <button class="absolute right-2 top-1/2 transform -translate-y-1/2"></button>
+
       <div class="flex w-max flex-shrink-0 items-center justify-end gap-6">
-        <span class="relative">
+        {/* home */}
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          class="relative"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-            class="h-8 w-8"
+            width="10"
+            height="10"
+            class="home-icon h-10 w-10 text-white transition-transform duration-300 hover:scale-110 hover:text-white"
+            fill="currentColor"
+            viewBox="0 0 50 50"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            ></path>
+            <path d="M 24.96 1.05 A 1 1 0 0 0 24.38 1.26 L 1.38 19.21 A 1 1 0 0 0 2.61 20.79 L 4 19.71 V46 A 1 1 0 0 0 5 47 H 18.83 A 1 1 0 0 0 19.16 47 H 30.83 A 1 1 0 0 0 31.16 47 H 45 A 1 1 0 0 0 46 46 V19.71 L 47.38 20.79 A 1 1 0 1 0 48.61 19.21 L 41 13.27 V6 H 35 V8.58 L 25.61 1.26 A 1 1 0 0 0 24.96 1.05 z M 25 3.32 L 44 18.15 V45 H 32 V26 H 18 V45 H 6 V18.15 L 25 3.32 z M 37 8 H 39 V11.71 L 37 10.15 V8 z M 20 28 H 30 V45 H 20 V28 z" />
           </svg>
-        </span>
-        <span class="relative">
+        </button>
+
+        {/* about  */}
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          class="relative p-2 bg-rgb(rgb(18 18 18)) rounded-full"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-            class="h-8 w-8 text-white"
+            class="w-10 h-10 text-white"
+            fill="currentColor"
+            viewBox="0 0 50 50"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-            ></path>
+            <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 25 11 A 3 3 0 0 0 22 14 A 3 3 0 0 0 25 17 A 3 3 0 0 0 28 14 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 22 23 L 23 23 L 23 36 L 22 36 L 21 36 L 21 38 L 22 38 L 23 38 L 27 38 L 28 38 L 29 38 L 29 36 L 28 36 L 27 36 L 27 21 L 26 21 L 22 21 L 21 21 z"></path>
           </svg>
-          <span className="absolute right-1 top-0 flex h-2.5 w-2.5 items-center justify-center rounded-full bg-green-500 p-1 text-white"></span>
-        </span>
+        </button>
+
+        {/* logout button */}
         <button
           onClick={handleclick}
-          className="hidden w-max items-center justify-center border-[1px] border-white p-3 text-center font-bold text-white md:inline-flex text-[20px]"
+          className="hidden w-max items-center justify-center border-[1px] border-white p-3 text-center font-bold text-white md:inline-flex text-[20px] hover:bg-white hover:text-black"
         >
           Logout
         </button>
