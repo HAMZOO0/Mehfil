49f678b9fab4059111dac27f478ba08b958461e2
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-14 22:14:15 +0500

    Update SidepofilePage.jsx

--- Front End/src/pages/SidepofilePage.jsx
+++ Front End/src/pages/SidepofilePage.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
 import {
+  LoadingSpinner,
 
   SideProfileView,
+
 } from "../components/index.js";
-const SideProfileViewPage =  () => {
+import { useStore } from "../Store/store.js";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SideProfileViewPage  () {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useStore();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
+  // Return null or loading spinner while the navigation is happening
+  if (!isAuthenticated) {
+    return <LoadingSpinner />; // You can use any loading component here
+  }
+
   return (
-    <div className="container mx-auto px-4 py-6">
-      <SideProfileView  />
-    </div>
+    <Layout
+      header={<Header />}
+    >
+     
+      <SideProfileView />
+    </Layout>
   );
 }
-export default SideProfileViewPage
