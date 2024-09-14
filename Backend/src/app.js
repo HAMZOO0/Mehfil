f69dcbda4c98b7a933aabe8d8c87097a7b927e64
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-14 10:55:26 -0400

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,15 +5,17 @@ import cors from "cors";
 const app = express();
 
 // CORS middleware configuration
-app.use(cors({
-  origin: 'https://mehfil-social-media.vercel.app'  // Your frontend URL
-}));
-
+app.use(
+  cors({
+    origin: 'https://mehfil-social-media.vercel.app', // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
 app.use(express.json({ limit: "16 kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16 kb" }));
 app.use(express.static("public")); // Serve static files from 'public' folder
 app.use(cookieparser()); // Parse cookies
-
 // Import routers
 import user_router from "./routers/user.route.js";
 import post_router from "./routers/post.route.js";
@@ -22,7 +24,6 @@ import follow_router from "./routers/follow.route.js";
 import comment_router from "./routers/comment.route.js";
 import bookmark_router from "./routers/bookmark.route.js";
 import healthcheck_router from "./routers/healthcheck.router.js";
-
 // Use routers
 app.use("/api/v1/users", user_router);
 app.use("/api/v1/posts", post_router);
@@ -31,5 +32,4 @@ app.use("/api/v1/follow", follow_router);
 app.use("/api/v1/comment", comment_router);
 app.use("/api/v1/bookmark", bookmark_router);
 app.use("/api/v1/healthcheck", healthcheck_router);
-
 export { app };
