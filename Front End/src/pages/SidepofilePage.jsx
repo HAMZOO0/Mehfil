3d358e0d1d8781c4b158096355bde044e2492a1f
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-14 22:09:18 +0500

    Update SidepofilePage.jsx

--- Front End/src/pages/SidepofilePage.jsx
+++ Front End/src/pages/SidepofilePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SideProfileView  from '../components/SideProfile/SideProfileBox.jsx'; // Adjust path if necessary
 
-export default function SideProfileViewPage () {
+const SideProfileViewPage =  () => {
   return (
     <div className="container mx-auto px-4 py-6">
       <SideProfileView  />
     </div>
   );
 }
+export default SideProfileViewPage
