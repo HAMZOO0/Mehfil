021f982f03242057f890259ad167a6a2e5f5865f
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-14 22:00:07 +0500

    Create FollowPage.jsx

---
+++ Front End/src/pages/FollowPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FollowBox from '../components/FollowBox'; // Adjust path if necessary
+
+function FollowPage() {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <FollowBox />
+    </div>
+  );
+}
