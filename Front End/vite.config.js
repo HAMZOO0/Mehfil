f4af9f51fa2bb821d93dc3f8f7aa73e22d937e95
Hamza Sajid <hamzakhan174722@gmail.com>
2024-09-14 04:31:38 -0400

    Update vite.config.js

--- Front End/vite.config.js
+++ Front End/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Proxy :{
-//   "/api" =  {
-//     target: "http://localhost:5000",
-
-// }}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mehfil-tau.vercel.app', // Your backend server URL
+        changeOrigin: true, // Change the origin of the request to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix when forwarding
+      },
+    },
+  },
 });
